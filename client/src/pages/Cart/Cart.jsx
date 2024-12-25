018ebbcc993f8c0cb27c6dd6dc458dcc06a8212a
Bittu Kumar <bittukumar8713@gmail.com>
2024-12-25 21:20:13 +0530

    update cart page

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import "./Cart.css";
-import { StoreContext } from "../../context/StoreContext";
+import { StoreContext } from "../../Context/StoreContext.jsx";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineDelete } from "react-icons/ai";
 import { url } from "../../config/config";
